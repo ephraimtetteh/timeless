a8d27e485aaad45f228dd49429df2be6f8fcf964
ephraim64 <tettehephraim.64@gmail.com>
2026-04-11 13:30:38 +0000

    feat: more product upload

--- src/constants/products.jsx
+++ src/constants/products.jsx
@@ -6,11 +6,128 @@ import confettiCoral from '/products/confetti-coral.jpeg'
 import confettiGorgeous from '/products/confetti-gorgeous.jpeg'
 import confettiNude from '/products/confetti-nude.jpeg'
 import confettiyou from '/products/confetti-you.jpeg'
+import dovemenGrape from '/products/dove-gofresh-grape.jpeg'
+import dovemenquava from '/products/dove-gofresh-quava.jpeg'
+import dovemen from '/products/dove-gofresh.jpeg'
+import dovemenCare from '/products/dove-men-dry-invisible.jpeg'
+import dovemenExtra from '/products/dove-men-extra-fresh.jpeg'
+import dovemenDry from '/products/dove-men-care.jpeg'
+import hayaati from '/products/hayaati.jpeg'
+import hayatti from '/products/hayatti-lattafa.jpeg'
+import khamrah from '/products/khamrah.jpeg'
+import nowWoman from '/products/now-woman.jpeg'
+import oudDeArabia from '/products/oud-de-arabia.jpeg'
+import queenOfRed from '/products/queen-of-red.jpeg'
+import ramz from '/products/ramz-lattafa.jpeg'
+import riggsAce from '/products/riggs-ace.jpeg'
+import riggsPatrol from '/products/riggs-patrol.jpeg'
+import riggsRustic from '/products/riggs-rustic.jpeg'
+import yara from '/products/yara.jpeg'
+
 
 
 
 
 export const productData = [
+  {
+    img: yara,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: riggsRustic,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: riggsPatrol,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: riggsAce,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: ramz,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: queenOfRed,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: oudDeArabia,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: nowWoman,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: khamrah,
+    title: "",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: hayatti,
+    title: "Hayatti Lattafa",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 1219876,
+  },
+  {
+    img: hayaati,
+    title: "Hayaati",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 100,
+    size: '100ml',
+    category: "men",
+    id: 19876,
+  },
   {
     img: rebel,
     title: "9pm-Rebel",
@@ -81,6 +198,60 @@ export const productData = [
     price: 700,
     size: '100ml',
     category: "men",
-    id: 37489,
+    id: 374898769,
+  },
+  {
+    img: dovemen,
+    title: "Dove Gofresh",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 37486359,
+  },
+  {
+    img: dovemenGrape,
+    title: "Dove Gofresh Grape",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 374098789,
+  },
+  {
+    img: dovemenquava,
+    title: "Dove Gofresh Quava",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 3748789,
+  },
+  {
+    img: dovemenCare,
+    title: "Dove Men Care",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 374978789,
+  },
+  {
+    img: dovemenExtra,
+    title: "Dove Men Extra Care",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 374978789,
+  },
+  {
+    img: dovemenDry,
+    title: "Dove Men Dry Invisible",
+    text: "A luminous fragrance that captures the essence of elegance and charm.",
+    price: 700,
+    size: '100ml',
+    category: "men",
+    id: 374978789,
   },
 ]
